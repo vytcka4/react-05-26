f9e1dd9290d3cf0e692036f1503269d580d974c3
vytcka4 <vytautas.freelance@gmail.com>
2022-06-01 00:33:30 +0300

    commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -102,7 +102,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         ></Input>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
